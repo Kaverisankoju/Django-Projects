55509582bbb9ac25566eb0547f6385a1ca5253df
Kaverisankoju <kaverisankoju2003@gmail.com>
2025-12-18 17:09:19 +0530

    deployment purpose

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'basic'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myproject' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
