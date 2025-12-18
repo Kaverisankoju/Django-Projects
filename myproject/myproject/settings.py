20f12768a3b99db042a4b169ffd0dfce5dfd5ce5
Kaverisankoju <kaverisankoju2003@gmail.com>
2025-12-18 17:19:29 +0530

    host is added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_24b*4ip5n!d)3hd3p44t6p#1_7rb@c1#2%gzhbby)mp6wqi@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-projects-ulps.onrender.com"]
 
 
 # Application definition
@@ -77,6 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
